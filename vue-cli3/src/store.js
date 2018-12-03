d2ad4ea450e23de787699c288b8f135ff1375bf3
ruanlin <ruanlin@123.com>
2018-12-03 14:51:07 +0800

    #更新cli3

--- vue-cli3/src/store.js
+++ vue-cli3/src/store.js
@@ -1,10 +1,32 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
+import * as type from "./mutation-types";
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    click: null,
+    resize: null
+  },
+  getters: {
+    onclick: state => state.click
+  },
+  mutations: {
+    [type.SET_EVENT](state, event) {
+      state[event.type] = event;
+      event.type === "click" ? alert("I am a mutations API") : null;
+    }
+  },
+  actions: {
+    triggerEvent({ commit }, event) {
+      return new Promise(resolve => {
+        setTimeout(() => {
+          commit(type.SET_EVENT, event);
+          resolve();
+        }, 1000);
+      });
+    }
+  }
 });

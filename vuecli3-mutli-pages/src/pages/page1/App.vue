46b1bbd9f4f7d2584b854de02a2a5b778bc68d65
ruanlin <ruanlin@123.com>
2018-12-05 10:41:31 +0800

    #vue多页应用例子

---
+++ vuecli3-mutli-pages/src/pages/page1/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <img src="../../assets/logo.png">
+    <div>
+      <p>
+        If Element is successfully added to this project, you'll see an
+        <code v-text="'<el-button>'"></code>
+        below
+      </p>
+      <el-button @click="toPage2">el-button</el-button>
+    </div>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "app",
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    toPage2() {
+      // window.location.assign("page2");
+      window.location.href = "page2";
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
